f2d9be953d809e4e411970b699e3f8272ab17536
Sean-Fuhrman <stfuhrma@ucsd.edu>
2024-05-30 18:26:46 -0700

    plot G image

---
+++ plot_G_image_results.py
@@ -0,0 +1,67 @@
+#%%
+import torch
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import dataloader
+import net_graph_h1
+# Read results
+#%%
+results = torch.load('G_image_results/training_data.pth')
+
+G_losses = results['G_losses']
+D_losses = results['D_losses']
+img_list = results['img_list']
+
+#%%
+
+
+# Plot the training losses
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+# %%
+img_length = len(img_list)
+plot_index = range(len(img_list))
+print(plot_index)
+# Plot the generated images
+for i in plot_index:
+    img = img_list[i].cpu().detach()
+    plt.figure(figsize=(10,10))
+    plt.axis("off")
+    plt.title("Generated Images at iteration {}".format(i * 500))
+    plt.imshow(np.transpose(img[0]))
+    plt.show()
+# %%
+
+
+segments = dataloader.load_segmented_images()
+sentence = dataloader.get_text_data()
+
+
+G = net_graph_h1.Generator()
+
+# Attempt with specific sentence
+
+#%%
+
+G.load_state_dict(torch.load('G_image_results/netG_1000.pth'))
+sen = sentence[10].unsqueeze(0).unsqueeze(2).unsqueeze(3).float()
+seg = segments[0].unsqueeze(0).float()
+nz = 80
+noise = torch.randn(1, nz, 1, 1)
+fake = G(noise, sen, seg)[0]
+
+plt.figure(figsize=(10,10))
+plt.axis("off")
+plt.title("Generated Image")
+plt.imshow(np.transpose(fake.detach().numpy()))
+plt.show()
+
+
+# %%
